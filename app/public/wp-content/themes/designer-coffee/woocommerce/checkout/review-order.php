f726bb2679d4b35d5bfe24e09f1f2be21c904396
bee20082000 <bee20082000@users.noreply.github.com>
2026-08-13 19:19:53 +0700

    Fix checkout metadata and totals alignment

--- app/public/wp-content/themes/designer-coffee/woocommerce/checkout/review-order.php
+++ app/public/wp-content/themes/designer-coffee/woocommerce/checkout/review-order.php
@@ -68,13 +68,13 @@ defined( 'ABSPATH' ) || exit;
   <tfoot>
     <tr class="cart-subtotal">
       <th><?php esc_html_e( 'Subtotal', 'woocommerce' ); ?></th>
-      <td colspan="2" style="text-align:right"><?php wc_cart_totals_subtotal_html(); ?></td>
+      <td style="text-align:right"><?php wc_cart_totals_subtotal_html(); ?></td>
     </tr>
 
     <?php foreach ( WC()->cart->get_coupons() as $code => $coupon ) : ?>
       <tr class="cart-discount coupon-<?php echo esc_attr( sanitize_title( $code ) ); ?>">
         <th><?php wc_cart_totals_coupon_label( $coupon ); ?></th>
-        <td colspan="2" style="text-align:right"><?php wc_cart_totals_coupon_html( $coupon ); ?></td>
+        <td style="text-align:right"><?php wc_cart_totals_coupon_html( $coupon ); ?></td>
       </tr>
     <?php endforeach; ?>
 
@@ -87,7 +87,7 @@ defined( 'ABSPATH' ) || exit;
     <?php foreach ( WC()->cart->get_fees() as $fee ) : ?>
       <tr class="fee">
         <th><?php echo esc_html( $fee->name ); ?></th>
-        <td colspan="2" style="text-align:right"><?php wc_cart_totals_fee_html( $fee ); ?></td>
+        <td style="text-align:right"><?php wc_cart_totals_fee_html( $fee ); ?></td>
       </tr>
     <?php endforeach; ?>
 
@@ -96,13 +96,13 @@ defined( 'ABSPATH' ) || exit;
         <?php foreach ( WC()->cart->get_tax_totals() as $code => $tax ) : ?>
           <tr class="tax-rate tax-rate-<?php echo esc_attr( sanitize_title( $code ) ); ?>">
             <th><?php echo esc_html( $tax->label ); ?></th>
-            <td colspan="2" style="text-align:right"><?php echo wp_kses_post( $tax->formatted_amount ); ?></td>
+            <td style="text-align:right"><?php echo wp_kses_post( $tax->formatted_amount ); ?></td>
           </tr>
         <?php endforeach; ?>
       <?php else : ?>
         <tr class="tax-total">
           <th><?php echo esc_html( WC()->countries->tax_or_vat() ); ?></th>
-          <td colspan="2" style="text-align:right"><?php wc_cart_totals_taxes_total_html(); ?></td>
+          <td style="text-align:right"><?php wc_cart_totals_taxes_total_html(); ?></td>
         </tr>
       <?php endif; ?>
     <?php endif; ?>
@@ -111,7 +111,7 @@ defined( 'ABSPATH' ) || exit;
 
     <tr class="order-total">
       <th><?php esc_html_e( 'Total', 'woocommerce' ); ?></th>
-      <td colspan="2" style="text-align:right"><?php wc_cart_totals_order_total_html(); ?></td>
+      <td style="text-align:right"><?php wc_cart_totals_order_total_html(); ?></td>
     </tr>
 
     <?php do_action( 'woocommerce_review_order_after_order_total' ); ?>
